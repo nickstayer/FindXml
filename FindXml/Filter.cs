1767e8cf681a2b63ac41acb782434928a562460e
ndubovskov <ndubovskov@DESKTOP-GVJB0RR>
2024-09-11 16:50:36 +0500

    Выборочно вернул подпапки с описанием ошибки

--- FindXml/Filter.cs
+++ FindXml/Filter.cs
@@ -10,6 +10,22 @@ public class Filter()
     public const string TYPE_NOT_VALID = "Файлы не прошедшие валидацию";
     public const string EXCLUDE_KEYWORD_STATEMENT_HAS_BEEN_UPLOADED = "уже было загружено в систему";
 
+    public static (string, string) ERROR_KEYWORD_NO_GUID = ("В заявлении не был передан GUID адреса", "Нет GUID адреса");
+    public static (string, string) ERROR_INCORRECT_PERIOD = ("Срок пребывания должен быть в диапазоне", "Некорректный срок пребывания");
+    public static (string, string) ERROR_CANT_CREATE_CASE = ("Невозможно создать дело", "Невозможно создать дело");//Отсутствует адрес принимающей стороны
+
+    public static (string, string) ERROR_NO_ADDRESS = ("ошибка ФЛК: отсутствует поле \"Адрес принимающей стороны\"", "Отсутствует адрес принимающей стороны");
+    public static (string, string) NO_EXPECTED_SYMBOLS = ("Поле не содержит цифр и символов", "Поле не содержит цифр и символов");
+
+    public static (string, string)[] ERROR_KEYWORDS = [
+        ERROR_KEYWORD_NO_GUID,
+        ERROR_INCORRECT_PERIOD,
+        ERROR_CANT_CREATE_CASE,
+        ERROR_NO_ADDRESS,
+        NO_EXPECTED_SYMBOLS,
+    ];
+
+
     public static string[] INCLUDE_TRANSFER_STATUS = [
         STATUS_ERRORS,
         STATUS_REPEAT,
@@ -23,6 +39,7 @@ public class Filter()
     public static string[] EXCLUDE_ERROR_DESCRIPTION_KEYWORDS = [
         EXCLUDE_KEYWORD_STATEMENT_HAS_BEEN_UPLOADED
     ];
+
     public static List<Transfer> Do(List<Transfer> transfers)
     {
         var result = transfers
@@ -73,8 +90,19 @@ public class Filter()
 
     public static string GetTargetFile(Transfer transfer, string sourceFile, string resultFolder)
     {
+        var errorFolderName = GetErrorFolderName(transfer);
         var fileName = Path.GetFileName(sourceFile);
-        var newFile = Path.Combine(resultFolder, transfer.AccountingType, transfer.TransferStatus, fileName);
+        var newFile = Path.Combine(resultFolder, transfer.AccountingType, transfer.TransferStatus, errorFolderName, fileName);
         return newFile;
+    }
+
+    private static string GetErrorFolderName(Transfer transfer)
+    {
+        foreach (var keyword in ERROR_KEYWORDS)
+        {
+            if (transfer.ErrorDescription.Contains(keyword.Item1))
+                return keyword.Item2;
+        }
+        return string.Empty;
     }
 }
